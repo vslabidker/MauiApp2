06aa44f495cb0715a28d3571aaefe2dbef594d3d
VladSl4 <vslabidker@gmail.com>
2025-03-31 12:40:44 +0300

    .

--- MauiApp2/MainPage.xaml.cs
+++ MauiApp2/MainPage.xaml.cs
@@ -1,25 +1,53 @@
-﻿namespace MauiApp2
+﻿using System;
+using RestSharp;
+using System.Threading.Tasks;
+
+namespace MauiApp2;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    public MainPage()
     {
-        int count = 0;
-
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        InitializeComponent();
     }
 
+    private async void OnFetchDataClicked(object sender, EventArgs e)
+    {
+
+        string query = SearchEntry.Text;
+
+        if (string.IsNullOrEmpty(query))
+        {
+            await DisplayAlert("Ошибка", "Введите запрос", "OK");
+            return;
+        }
+
+        var client = new RestClient("https://api.apilayer.com/spoonacular/recipes/complexSearch");
+        var request = new RestRequest();
+        request.AddParameter("query", query); 
+        request.AddHeader("apikey", "lBk76uWoDkHbjMQ9jSUuuxQZOGTglsGj");
+
+        try
+        {
+            
+            var response = await client.ExecuteAsync(request); 
+
+            Console.WriteLine($"Status Code: {response.StatusCode}");
+            Console.WriteLine($"Response Content: {response.Content}");
+
+            if (response.IsSuccessful)
+            {
+                await DisplayAlert("Ответ API", response.Content, "OK");
+            }
+            else
+            {
+                await DisplayAlert("Ошибка", "Не удалось получить данные с API", "OK");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+            await DisplayAlert("Ошибка", "Произошла ошибка при запросе", "OK");
+        }
+    }
 }
